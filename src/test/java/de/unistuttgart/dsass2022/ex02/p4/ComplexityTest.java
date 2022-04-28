56cdb1a6e4e2510253956952dbf3fce83c4e5473
marvin <marvinmai@posteo.de>
2022-04-28 18:48:40 +0200

    Add complexity test

--- src/test/java/de/unistuttgart/dsass2022/ex02/p4/ComplexityTest.java
+++ src/test/java/de/unistuttgart/dsass2022/ex02/p4/ComplexityTest.java
@@ -4,5 +4,25 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ComplexityTest {
-	
+
+    @Test
+    public void testFirst() {
+        int n = 19;
+
+        assertEquals(Complexity.isDoneBetter1(n), Complexity.couldBeBetter1(n));
+    }
+
+    @Test
+    public void testSecond() {
+        int n = 19;
+
+        assertEquals(Complexity.isDoneBetter2(n), Complexity.couldBeBetter2(n));
+    }
+
+    @Test
+    public void testThird() {
+        int n = 19;
+
+        assertEquals(Complexity.isDoneBetter3(n), Complexity.couldBeBetter3(n));
+    }
 }
