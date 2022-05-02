a76dd2912c3fcc674892765fdf9a19b6b133ed8f
marvin <marvinmai@posteo.de>
2022-05-02 12:52:25 +0200

    Add test for SimpleList

--- src/test/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListTest.java
+++ src/test/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListTest.java
@@ -1,11 +1,26 @@
 package de.unistuttgart.dsass2022.ex02.p5;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import java.util.Map;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class SimpleListTest {
+
+    private final Map<SimpleList<Integer>, SimpleList<Integer>> testListCouples;
+
+    public SimpleListTest() {
+        this.testListCouples = TestListData.getTestListCouples();
+    }
+
+    @Test
+    public void testSimpleList() {
+        for (Map.Entry<SimpleList<Integer>, SimpleList<Integer>> couple: testListCouples.entrySet()) {
+            couple.getKey().sort();
+
+            assertEquals(couple.getKey(), couple.getValue());
+        }
+    }
 
 }
