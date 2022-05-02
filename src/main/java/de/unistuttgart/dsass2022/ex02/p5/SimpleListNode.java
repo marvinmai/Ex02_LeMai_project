b9c4df45125fc81a5fc2a44966dad99f0e81be71
marvin <marvinmai@posteo.de>
2022-05-02 12:26:52 +0200

    Implement SimpleListNode

--- src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListNode.java
+++ src/main/java/de/unistuttgart/dsass2022/ex02/p5/SimpleListNode.java
@@ -1,26 +1,28 @@
 package de.unistuttgart.dsass2022.ex02.p5;
 
 public class SimpleListNode<T extends Comparable<T>> implements ISimpleListNode<T>{
-	
+
+	private T element;
+	private ISimpleListNode<T> next;
 
 	@Override
 	public T getElement() {
-		return null;
+		return element;
 	}
 
 	@Override
 	public void setElement(T element) {
-
+		this.element = element;
 	}
 
 	@Override
 	public ISimpleListNode<T> getNext() {
-		return null;
+		return this.next;
 	}
 
 	@Override
 	public void setNext(ISimpleListNode<T> node) {
-
+		this.next = node;
 	}
 
 }
